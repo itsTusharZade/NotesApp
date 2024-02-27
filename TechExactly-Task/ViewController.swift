596e95dfe7e0d0372d04c2ced33565a1131cf4dc
tmz2792 <137619918+tmz2792@users.noreply.github.com>
2024-02-27 22:06:35 +0530

    Initial Commit

---
+++ TechExactly-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechExactly-Task
+//
+//  Created by Tushar Zade on 27/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
